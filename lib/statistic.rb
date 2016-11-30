8411aaaf64d8cfa9066ef3c629684bd35ef82518
frodo <eugene@moscow.ctm.ru>
2016-11-30 17:59:52 +0300

    fix up- and down-time

--- lib/statistic.rb
+++ lib/statistic.rb
@@ -3,26 +3,33 @@ module HostChecker
   class Statistic
     attr_reader :up_at
     attr_reader :down_at
-    attr_accessor :attempt_count
+    attr_reader :attempt_count
     def initialize
       @attempt_count = 0
     end
     def up(time=Time.now)
-      self.attempt_count = 0
-      down_at = nil
-      up_at = time
+      @attempt_count = 0
+      @down_at = nil
+      @up_at ||= time
       nil
     end
     def down(time=Time.now)
-      up_at = nil
-      down_at ||= time
-      self.attempt_count +=1
+      @up_at = nil
+      @down_at ||= time
+      @attempt_count +=1
     end
     def down_time
-      Time.now - down_at
+      Time.now - @down_at
     end
     def is_down?
-      !down_at.nil?
+      !@down_at.nil?
+    end
+    def to_s
+      if is_down?
+        "down at:#{down_at}; attempt count: #{attempt_count} down time #{down_time}"
+      else
+        "is up"
+        end
     end
   end
 end
